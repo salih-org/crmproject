4c72d752b6e39feed5a509c46a8d9064946f04a4
Salih Cantekin <salih.cantekin@etstur.com>
2020-10-11 02:25:04 +0300

    .gitignore updated

---
+++ ApiGateways/Crm.API.Gateway/Extensions/HealthCheckConfigurationExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crm.API.Gateway.Extensions
+{
+    public static class HealthCheckConfigurationExtension
+    {
+        public static IApplicationBuilder UseCustomHealthCheck(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/health", getHealthCheckOptions());
+
+            return app;
+        }
+
+        private static HealthCheckOptions getHealthCheckOptions()
+        {
+            return new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    await context.Response.WriteAsync("OK");
+                }
+            };
+        }
+    }
+}
--- ApiGateways/Crm.API.Gateway/Startup.cs
+++ ApiGateways/Crm.API.Gateway/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Crm.API.Gateway.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 namespace Crm.API.Gateway
@@ -27,19 +29,25 @@ namespace Crm.API.Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHealthChecks();
+
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseOcelot();
+            
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            app.UseCustomHealthCheck();
 
             app.UseRouting();
 
@@ -49,6 +57,8 @@ namespace Crm.API.Gateway
             {
                 endpoints.MapControllers();
             });
+
+            await app.UseOcelot();
         }
     }
 }
